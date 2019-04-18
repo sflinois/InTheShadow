ed8fca4a0227621d80c64ebe87f697596ec29b80
Sylvain FLINOIS <sflinois@e2r9p2.42.fr>
2019-04-18 17:11:12 +0200

    InTheShadow

---
+++ InTheShadows/Assets/Scripts/Camera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+	private GameObject cam;
+	private Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+	}
+	
+	public void setMenu(){
+		anim.SetBool("isMenu", true);
+		anim.SetBool("isLevel", false);
+	}
+
+	public void setLevels(){
+		anim.SetBool("isMenu", false);
+		anim.SetBool("isLevel", true);
+	}
+}
