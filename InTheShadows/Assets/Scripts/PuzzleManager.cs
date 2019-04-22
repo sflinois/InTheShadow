7acbb9dd32bf772563b7755602d93280cdd28c9a
BuildTools <unconfigured@null.spigotmc.org>
2019-04-22 17:32:55 +0200

    added level difficulty

--- InTheShadows/Assets/Scripts/PuzzleManager.cs
+++ InTheShadows/Assets/Scripts/PuzzleManager.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour {
     
-    public PuzzleObject puzzleObject;
+    public PuzzleObject[] puzzleObjects;
     public Canvas menuCanvas;
-    private bool isMouseDrag = false;
+    public int pDifficulty;
+    private int puzzleIndex = -1;
+    private int selectMode = 0;
     private bool isSolved = false;
 
     void Update()
     {
         if (!isSolved)
         {
-            handleMouse();
-            if (puzzleObject.isSolved())
+            if (!menuCanvas.enabled)
             {
                 isSolved = true;
                 menuCanvas.enabled = true;
+                for (int i = 0; i < puzzleObjects.Length; i++)
+                {
+                    if (!puzzleObjects[i].isSolved())
+                    {
+                        isSolved = false;
+                        menuCanvas.enabled = false;
+                        break;
+                    }
+                }
             }
-
+            handleMouse();
         }
     }
 
@@ -32,20 +42,72 @@ public class PuzzleManager : MonoBehaviour {
     {
         float drag;
 
+
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Debug.Log("Keydown escape");
             menuCanvas.enabled = !menuCanvas.enabled;
         }
+        if (menuCanvas.enabled)
+            return;
+        
+        // mode selection
         if (Input.GetMouseButtonDown(0))
-            isMouseDrag = true;
-        if (Input.GetMouseButtonUp(0))
-            isMouseDrag = false;
-        if (isMouseDrag)
         {
-            drag = Input.GetAxis("Mouse X");
-            if (drag > 0.05f || drag < -0.05f)
-                puzzleObject.rotate(0, drag, 0);
+            if (Input.GetKey(KeyCode.O) && pDifficulty >= 3)
+                selectMode = 3;
+            else if (Input.GetKey(KeyCode.P) && pDifficulty >= 2)
+                selectMode = 2;
+            else if (pDifficulty >= 1)
+                selectMode = 1;
+        }
+
+        if (selectMode > 0)
+        {
+            PuzzleObject tmp;
+            int tmpIndex = 0;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                tmp = hit.transform.gameObject.GetComponent<PuzzleObject>(); 
+                if (tmp)
+                {
+                    for (int i = 0; i < puzzleObjects.Length; i++)
+                    {
+                        if (puzzleObjects[i] == tmp)
+                            break;
+                        tmpIndex++;
+                    }
+                    puzzleIndex = tmpIndex < puzzleObjects.Length ? tmpIndex : -1;
+                }
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            puzzleIndex = -1;
+            selectMode = 0;
+        }
+        if (puzzleIndex >= 0 && selectMode > 0)
+        {
+            if (selectMode == 1)
+            {
+                drag = Input.GetAxis("Mouse X");
+                if (drag > 0.05f || drag < -0.05f)
+                    puzzleObjects[puzzleIndex].rotate(0, drag, 0);
+            }
+            else if (selectMode == 2)
+            {
+                drag = Input.GetAxis("Mouse Y");
+                if (drag > 0.05f || drag < -0.05f)
+                    puzzleObjects[puzzleIndex].rotate(0, 0, drag);
+            }
+            else if (selectMode == 1)
+            {
+//                drag = Input.GetAxis("Mouse X");
+//                if (drag > 0.05f || drag < -0.05f)
+//                    puzzleObjects[puzzleIndex].rotate(0, drag, 0);
+            }
         }
     }
 }
